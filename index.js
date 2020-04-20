fec134fa10bc7a7f376681752617b32f30c07c6a
abdukhashimov <abdukhashimov@yandex.ru>
2020-04-20 09:26:26 +0500

    create and get is implemented

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 const express = require("express");
 
 const app = express();
-const port = 3000;
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(courses);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => {
+    return c.id === parseInt(req.params.id);
+  });
+
+  if (!course)
+    res.status(404).send("The course with the given id was not found");
+
+  res.status(200).send(course);
+});
+
+app.post("/api/courses", (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
